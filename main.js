478c6e111bef7036f06e06aeee480dce5a750922
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-13 23:39:40 +0000

    Update JavaScript

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 let timer = document.getElementById("timer");
+let hours = document.getElementById("hours");
+let minutes = document.getElementById("minutes");
+let seconds = docuement.getElementById("seconds");
+let ampm = document.getElementById("ampm");
+let startstop = documene.getElementById("startstop");
+
+let currentTime;
+
+function showClockTime() {
+    let now = new Date();
+    currentTime = now.toLocaleTimeString();
+    timer.textContent = currentTime;
+}
+
+showClockTime();
