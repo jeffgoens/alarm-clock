8ddc4a544570946ad8252d6866d0a1eddf279550
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-16 02:51:38 +0000

    Updates

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
-let timer = document.getElementById("timer");
-let hours = document.getElementById("hours");
-let minutes = document.getElementById("minutes");
-let seconds = document.getElementById("seconds");
-let ampm = document.getElementById("ampm");
-let startstop = document.getElementById("startstop");
-
+let clock = document.getElementById("clock");
 let currentTime;
+let alarmTime = null;
+let alarmTimeout = null;
 
 function showClockTime() {
     let now = new Date();
     currentTime = now.toLocaleTimeString();
-    timer.textContent = currentTime;
+
+    clock.textContent = currentTime;
     setTimeout(showClockTime, 1000);
+
 }
 
 showClockTime();
+
+function setAlarmTime(value) {
+    alarmTime = value;
+}
+
+function setAlarm() {
+    if(alarmTime) {
+        const current = new Date();
+        const timeToAlarm = new Date(alarmTime);
+
+        if (timeToAlarm > current) {
+            const timeout = timeToAlarm.getTime() - current.getTime();
+            alarmTimeout = setTimeout(() => timeout);
+            alert('Alarm set');
+        }
+    }
+}
+
+function clearAlarm() {
+    if (alarmTimeout) {
+        clearTimeout(alarmTimeout);
+        alert('Alarm cleared');
+    }
+}
